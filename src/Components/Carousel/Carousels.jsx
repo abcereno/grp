330285caaa04b792a844fa828c7535e53163c690
Anthony <cereno.abc@gmail.com>
2024-03-06 12:28:20 +0800

    fixed responsiveness

--- src/Components/Carousel/Carousels.jsx
+++ src/Components/Carousel/Carousels.jsx
@@ -9,21 +9,21 @@ function Carousels() {
       <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
       <Carousel>
       <Carousel.Item>
-        <img src={pic} alt="" text="First slide" />
+        <img src={pic} alt="" text="First slide" className="img-fluid" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={pic} alt="" text="second slide" />
+      <img src={pic} alt="" text="second slide" className="img-fluid" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={pic} alt="" text="third slide" />
+      <img src={pic} alt="" text="third slide" className="img-fluid" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
--- src/Components/Process/Process.jsx
+++ src/Components/Process/Process.jsx
@@ -19,6 +19,7 @@ const Process = () => {
                     ))}
                 </ol>
             </Col>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit numquam laboriosam minus molestias ut ab eos inventore molestiae, illo officia laudantium voluptates. Qui soluta est in officiis distinctio consequatur earum culpa sint quis, totam suscipit quam odit magnam laudantium adipisci maiores nisi voluptas a sunt. Officiis nostrum ipsa maiores eveniet. Mollitia delectus suscipit iusto quibusdam reiciendis molestias ipsum deleniti, ab laudantium odio odit facilis fuga, architecto, fugit ipsa aliquid dolorum dolore natus est cumque. Accusantium eius commodi consequatur sed error fugit cupiditate perferendis autem reprehenderit, deleniti ipsa vel dolore adipisci ad modi repudiandae ab vero quis, amet doloribus quam eligendi.</p>
         </Row>
     )
 }
